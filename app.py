55aca30d6554c4ba0cae6b471c8fed64c60476e6
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-03 21:34:56 -0500

    wrote program

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import random
 
 def compare_nums(nums, user_guess):
     cowbull = [0,0]
-    for i in range(len(num)):
+    for i in range(len(nums)):
         if num[i] == user_guess[i]:
             cowbull[1]+=1
         else:
@@ -22,12 +22,18 @@ if __name__ == "__main__":
     print("Type 'exit' at any prompt to exit the game")
 
     while playing:
-        user_guess = input("Guess a number")
-        if user_guess == "exit":
-            break
+        user_guess = input("Guess a number: ")
+        if user_guess == "exit": 
+            break #user quit game 
         cowbullcount = compare_nums(nums, user_guess)
         guesses += 1
 
         print("You have "+ str(cowbullcount[0] + " cows, and " + str(cowbullcount[1])+ " bulls."))
 
-        
+        if cowbullcount[1] == 4:
+            playing = False
+            print("You've won. This time.")
+            break #game ends. user won. 
+        else:
+            print("WRONG. Guess again.")
+
